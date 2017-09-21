d87a0967b528ad679744380aa77f0ce46cd10140
vic <victor.rocha@apisys.com.br>
2017-09-20 21:59:18 -0300

    lancamentos e navbar layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import {InputTextModule} from 'primeng/components/inputtext/inputtext';
+import {ButtonModule} from 'primeng/components/button/button';
+import {DataTableModule} from 'primeng/components/datatable/datatable';
+import {TooltipModule} from 'primeng/components/tooltip/tooltip';
+import {SlideMenuModule} from 'primeng/components/slidemenu/slidemenu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    DataTableModule,
+    TooltipModule,
+    SlideMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/components/common/api';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  items: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'File',
+        items: [{
+          label: 'New',
+          icon: 'fa-plus',
+          items: [
+            {label: 'Project'},
+            {label: 'Other'},
+          ]
+        },
+          {label: 'Open'},
+          {label: 'Quit'}
+        ]
+      },
+      {
+        label: 'Edit',
+        icon: 'fa-edit',
+        items: [
+          {label: 'Undo', icon: 'fa-mail-forward'},
+          {label: 'Redo', icon: 'fa-mail-reply'}
+        ]
+      }
+    ];
+
+  }
+}
